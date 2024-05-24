e3c80517d00caf3ab73b22b74f8bc5d6f678c1e3
ButteryStancakes <jlj3322@gmail.com>
2024-05-24 02:02:35 -0400

    Fixed stinger being able to play multiple times when using different entrances

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+# v1.0.2
+- Fixed stinger being allowed to reply multiple times through each entrance
 # v1.0.1
 - Added a config setting to allow stingers to replay randomly if entering the building interrupts the outdoor ambient music tracks
 - Tried to fix compatibility with modded interiors (you shouldn't be blocked from teleporting into the building anymore)
--- Plugin.cs
+++ Plugin.cs
@@ -11,7 +11,7 @@ namespace RevisitStingers
     [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        const string PLUGIN_GUID = "butterystancakes.lethalcompany.revisitstingers", PLUGIN_NAME = "Revisit Stingers", PLUGIN_VERSION = "1.0.1";
+        const string PLUGIN_GUID = "butterystancakes.lethalcompany.revisitstingers", PLUGIN_NAME = "Revisit Stingers", PLUGIN_VERSION = "1.0.2";
         internal static ConfigEntry<bool> configInterruptMusic;
         internal static new ManualLogSource Logger;
 
@@ -36,14 +36,19 @@ namespace RevisitStingers
     {
         [HarmonyPatch(typeof(EntranceTeleport), "TeleportPlayer")]
         [HarmonyPrefix]
-        static void TryReplayStinger(EntranceTeleport __instance, ref bool ___checkedForFirstTime)
+        static void EntranceTeleportPreTeleportPlayer(EntranceTeleport __instance, ref bool ___checkedForFirstTime)
         {
+            if (ReplayStinger.beenInsideThisRound)
+                return;
+
+            ReplayStinger.beenInsideThisRound = true;
+
             if (!___checkedForFirstTime && __instance.firstTimeAudio != null && ES3.Load($"PlayedDungeonEntrance{__instance.dungeonFlowId}", "LCGeneralSaveData", false) && (!StartOfRound.Instance.isChallengeFile || !ES3.Load("FinishedChallenge", "LCChallengeFile", false)))
             {
                 try
                 {
                     if (ReplayStinger.StingerShouldReplay())
-                        __instance.StartCoroutine(DelayedStinger(__instance.firstTimeAudio));
+                        __instance.StartCoroutine(ReplayStinger.DelayedStinger(__instance.firstTimeAudio));
                 }
                 catch (System.Exception e)
                 {
@@ -53,15 +58,25 @@ namespace RevisitStingers
             }
         }
 
-        static IEnumerator DelayedStinger(AudioClip stinger)
+        [HarmonyPatch(typeof(StartOfRound), "SetShipReadyToLand")]
+        [HarmonyPostfix]
+        static void StartOfRoundPostSetShipReadyToLand()
         {
-            yield return new WaitForSeconds(0.6f);
-            HUDManager.Instance.UIAudio.PlayOneShot(stinger);
+            ReplayStinger.beenInsideThisRound = false;
+        }
+
+        [HarmonyPatch(typeof(StartOfRound), "Awake")]
+        [HarmonyPostfix]
+        static void StartOfRoundPostAwake()
+        {
+            ReplayStinger.beenInsideThisRound = false;
         }
     }
 
     internal class ReplayStinger
     {
+        internal static bool beenInsideThisRound;
+
         internal static bool StingerShouldReplay()
         {
             if (Plugin.configInterruptMusic.Value && SoundManager.Instance.musicSource.isPlaying && SoundManager.Instance.musicSource.time > 5f && Random.value <= 0.25f)
@@ -73,6 +88,12 @@ namespace RevisitStingers
                 return true;
 
             return false;
+        }
+
+        internal static IEnumerator DelayedStinger(AudioClip stinger)
+        {
+            yield return new WaitForSeconds(0.6f);
+            HUDManager.Instance.UIAudio.PlayOneShot(stinger);
         }
     }
 }
