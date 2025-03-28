5411ccada4250b3c430172f305b9be1fe67c74fe
ButteryStancakes <jlj3322@gmail.com>
2025-03-28 13:28:01 -0400

    LobbyCompatibility + inverse teleport fixes

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Bootstrap;
 using BepInEx.Logging;
 using BepInEx.Configuration;
 using HarmonyLib;
@@ -8,15 +9,24 @@ using UnityEngine;
 namespace RevisitStingers
 {
     [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    [BepInDependency(GUID_LOBBY_COMPATIBILITY, BepInDependency.DependencyFlags.SoftDependency)]
     public class Plugin : BaseUnityPlugin
     {
-        const string PLUGIN_GUID = "butterystancakes.lethalcompany.revisitstingers", PLUGIN_NAME = "Revisit Stingers", PLUGIN_VERSION = "1.2.2";
+        internal const string PLUGIN_GUID = "butterystancakes.lethalcompany.revisitstingers", PLUGIN_NAME = "Revisit Stingers", PLUGIN_VERSION = "1.3.0";
         internal static ConfigEntry<float> configMaxRarity, configInterruptMusic, configFallbackChance;
         internal static new ManualLogSource Logger;
+
+        const string GUID_LOBBY_COMPATIBILITY = "BMX.LobbyCompatibility";
 
         void Awake()
         {
             Logger = base.Logger;
+
+            if (Chainloader.PluginInfos.ContainsKey(GUID_LOBBY_COMPATIBILITY))
+            {
+                Logger.LogInfo("CROSS-COMPATIBILITY - Lobby Compatibility detected");
+                LobbyCompatibility.Init();
+            }
 
             AcceptableValueRange<float> percentage = new(0f, 1f);
             string chanceHint = " (0 = never, 1 = guaranteed, or anything in between - 0.5 = 50% chance)";
@@ -57,6 +67,9 @@ namespace RevisitStingers
 
             ReplayStinger.beenInsideThisRound = true;
 
+            if (!__instance.isEntranceToBuilding)
+                return;
+
             // don't interfere with vanilla behavior
             if (___checkedForFirstTime || !ES3.Load($"PlayedDungeonEntrance{RoundManager.Instance.currentDungeonType}", "LCGeneralSaveData", false))
                 return;
@@ -64,7 +77,7 @@ namespace RevisitStingers
             try
             {
                 AudioClip stinger = RoundManager.Instance.dungeonFlowTypes[RoundManager.Instance.currentDungeonType].firstTimeAudio;
-                if (ReplayStinger.StingerShouldReplay())
+                if (stinger != null && ReplayStinger.StingerShouldReplay())
                     __instance.StartCoroutine(ReplayStinger.DelayedStinger(stinger));
             }
             catch (System.Exception e)
@@ -85,6 +98,30 @@ namespace RevisitStingers
         static void StartOfRoundPostAwake()
         {
             ReplayStinger.beenInsideThisRound = false;
+        }
+
+        [HarmonyPatch(typeof(ShipTeleporter), "TeleportPlayerOutWithInverseTeleporter")]
+        [HarmonyPostfix]
+        static void PostTeleportPlayerOutWithInverseTeleporter(int playerObj)
+        {
+            if (StartOfRound.Instance.allPlayerScripts[playerObj] != GameNetworkManager.Instance.localPlayerController || ReplayStinger.beenInsideThisRound)
+                return;
+
+            ReplayStinger.beenInsideThisRound = true;
+
+            try
+            {
+                AudioClip stinger = RoundManager.Instance.dungeonFlowTypes[RoundManager.Instance.currentDungeonType].firstTimeAudio;
+                if (stinger != null && (ReplayStinger.StingerShouldReplay() || !ES3.Load($"PlayedDungeonEntrance{RoundManager.Instance.currentDungeonType}", "LCGeneralSaveData", false)))
+                {
+                    StartOfRound.Instance.StartCoroutine(ReplayStinger.DelayedStinger(stinger)); // in case inverse is stored
+                    ES3.Save($"PlayedDungeonEntrance{RoundManager.Instance.currentDungeonType}", true, "LCGeneralSaveData");
+                }
+            }
+            catch (System.Exception e)
+            {
+                Plugin.Logger.LogWarning($"Ran into an error attempting to replay stinger - this is likely due to an incompatibility with modded content\n{e}");
+            }
         }
     }
 
